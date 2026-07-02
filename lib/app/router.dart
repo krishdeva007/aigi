bde0d212228472a0e7477340805469d97ecd5afc
krishdeva007 <eiswargovi4@gmail.com>
2026-07-03 01:34:19 +0530

    feat(auth): add splash screen and login navigation

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,23 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:aigi/features/auth/presentation/pages/login_page.dart';
+import 'package:aigi/features/auth/presentation/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) {
-        return const Scaffold(
-          body: Center(
-            child: Text(
-              "AIGI",
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        );
-      },
+      builder: (context, state) => const SplashPage(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
     ),
   ],
 );
